07225d18a84b6f7a39f6a13d939d703fe1cb5430
Dustin G <42304513+DustinThewind505@users.noreply.github.com>
2020-02-04 14:19:34 -0700

    added King Vegeta logo and CardContainer

--- kingvegeta/src/App.js
+++ kingvegeta/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardContainer from './components/CardContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://dbz.space/asset/global/character/card/2000120/card_2000120_character.png" className="App-logo" alt="King Vegeta" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         King Vegeta
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <CardContainer />
     </div>
   );
 }
