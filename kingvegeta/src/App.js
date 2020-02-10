c3079823f4ea919c919de8902353b49d6abba63f
unknown <Dustin.Guillen14@yahoo.com>
2020-02-09 20:45:21 -0700

    made Evil Morty events in CharacterCard, made background clickable in App.js

--- kingvegeta/src/App.js
+++ kingvegeta/src/App.js
@@ -1,43 +1,24 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import CardContainer from './components/CardContainer';
 import './App.css';
 
-// const Image = document.querySelector("img");
-// Image.addEventListener("mouseover", () => Image.src = "https://www.bing.com/th/id/OIP.Jm_OAMZ4biSSznXhD7kNxwHaHa?pid=Api&rs=1");
-// Image.addEventListener("mouseleave", () => Image.src="https://4.bp.blogspot.com/-2bZ4P4_tBBQ/WlvmPKrzEmI/AAAAAAABO1g/L62SqX666uYDr0DXhva6uWiKH4cOo8_SwCKgBGAs/s1600/58f37701a4fa116215a9240c.png");
-// const topImg = document.createElement("img");
-// topImg.src = "https://www.bing.com/th/id/OIP.Jm_OAMZ4biSSznXhD7kNxwHaHa?pid=Api&rs=1";
-// topImg.className = "App-logo";
-
-// const theHeader = document.querySelector("header");
-// console.log(topImg)
-
-// useEffect(()=> {
-//   function handleClick(event){
-//     event.preventDefault();
-//     console.log("mouseclick")
-//   }
-// }, [])
-function handleMouseOver(event){
-  event.preventDefault();
-  
-  event.target.src = "https://retohercules.com/images/morty-face-png-3.png";
-}
-
-function handleMouseLeave(event){
-  event.preventDefault();
-  
-  event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"
-}
+// function bgColor(){
+//   setBackground(!background);
+// }
 
 function App() {
+  const [background, setBackground] = useState(false);
+
+  function bgColor(){
+    setBackground(!background);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}/>
+      <header className="App-header" style={background ? {backgroundColor:"blue"} : {backgroundColor:"#282c34"}} onClick={() => setBackground(!background)}>
+        <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"}/>
         
-        <h2>
+        <h2 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
          "Wubba Lubba Dub Dub!" -Rick Sanchez
         </h2>
       </header>
--- kingvegeta/src/components/CharacterCard.js
+++ kingvegeta/src/components/CharacterCard.js
@@ -9,8 +9,8 @@ function CharacterCard(props) {
     return (
         <div className="character-list">
             <div key={props.id}>
-                <h2>{props.name}</h2>
-                <img src={props.image} className='test'/>
+                <h2 onMouseOver={(event) => event.target.textContent = "Evil Morty 🚫"}>{props.name}</h2>
+                <img src={props.image} className='test' onMouseOver={(event) => event.target.src = "https://rickandmortyapi.com/api/character/avatar/118.jpeg"} onMouseLeave={(event) => event.target.src = props.image}/>
                 <h3>Location: {props.location.name}</h3>
                 <h4>DOA: {props.status}</h4>
             </div>
