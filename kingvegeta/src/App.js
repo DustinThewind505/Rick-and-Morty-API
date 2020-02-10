b9c7ee9d9e7e83a868a3667716d6c18862455585
unknown <Dustin.Guillen14@yahoo.com>
2020-02-09 21:21:29 -0700

    click App.js header for portal. hover over cards app.css

--- kingvegeta/src/App.js
+++ kingvegeta/src/App.js
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from 'react';
 import CardContainer from './components/CardContainer';
 import './App.css';
 
-// function bgColor(){
-//   setBackground(!background);
-// }
-
 function App() {
   const [background, setBackground] = useState(false);
 
@@ -15,16 +11,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header" style={background ? {backgroundColor:"blue"} : {backgroundColor:"#282c34"}} onClick={() => setBackground(!background)}>
-        <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"}/>
-        
+      <header className="App-header" style={background ? { background: 'url("https://wallpaperboat.com/wp-content/uploads/2019/04/rick-and-morty-wallpaper-portal-wallpaper-001.jpg")' } : { background: "#282c34" }} onClick={bgColor}>
+
+        <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"} />
+
         <h2 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
-         "Wubba Lubba Dub Dub!" -Rick Sanchez
+          "Wubba Lubba Dub Dub!" -Rick Sanchez
         </h2>
+
       </header>
-      
-        
-      
       <CardContainer />
     </div>
   );
