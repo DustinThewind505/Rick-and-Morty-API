8dde784e973b631d6eddd93f8cd95edc8a7a5af6
unknown <Dustin.Guillen14@yahoo.com>
2020-03-21 22:48:25 -0600

    changed font for Dead or Alive/ styled tablet version

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -12,7 +12,7 @@ function CharacterCard(props) {
                 <h2 onMouseOver={(event) => event.target.textContent = "Evil Morty 🚫"} onMouseLeave={(event) => event.target.textContent = props.name}>{props.name}</h2>
                 <img src={props.image} className='test' onMouseOver={(event) => event.target.src = "https://rickandmortyapi.com/api/character/avatar/118.jpeg"} onMouseLeave={(event) => event.target.src = props.image} alt={props.name}/>
                 <h3>Location: {props.location.name}</h3>
-                <h4>DOA: {props.status}</h4>
+                <h4>DOA: <span className="doa">{props.status}</span></h4>
             </div>
         </div>
 
