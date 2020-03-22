be5a851dcdfa2b67036a62a1a5f3c584ab42b92b
unknown <Dustin.Guillen14@yahoo.com>
2020-03-21 23:21:46 -0600

    polished css

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
 
         <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"} />
 
-        <h2 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
+        <h1 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
           "Wubba Lubba Dub Dub!" -Rick Sanchez
-        </h2>
+        </h1>
 
       </header>
       <div className="navbar">
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -11,7 +11,7 @@ function CharacterCard(props) {
             <div key={props.id}>
                 <h2 onMouseOver={(event) => event.target.textContent = "Evil Morty 🚫"} onMouseLeave={(event) => event.target.textContent = props.name}>{props.name}</h2>
                 <img src={props.image} className='test' onMouseOver={(event) => event.target.src = "https://rickandmortyapi.com/api/character/avatar/118.jpeg"} onMouseLeave={(event) => event.target.src = props.image} alt={props.name}/>
-                <h3>Location: {props.location.name}</h3>
+                <h4>Location: {props.location.name}</h4>
                 <h4>DOA: <span className="doa">{props.status}</span></h4>
             </div>
         </div>
