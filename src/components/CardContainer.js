af41bdca98818edc28da7016871c0daa36f51bc5
unknown <Dustin.Guillen14@yahoo.com>
2020-03-22 22:38:01 -0600

    styled navbar and search forms

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -11,8 +11,8 @@ function CardContainer() {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([])
 
-    function handleChanges(event){
-            setSearchTerm(event.target.value)
+    function handleChanges(event) {
+        setSearchTerm(event.target.value)
     }
 
     useEffect(() => {
@@ -22,7 +22,7 @@ function CardContainer() {
                 console.log(response)
                 const results = response.data.results.filter(element =>
                     element.name.toLowerCase().includes(searchTerm.toLowerCase())
-                    );
+                );
 
                 setSearchResults(results)
                 //setRickMorty(response.data.results);
@@ -35,38 +35,42 @@ function CardContainer() {
 
 
     return (
-        <div className="container">
-            <span>Character: |</span>
-            <input
-                type='text'
-                placeholder='Search'
-                value={searchTerm}
-                onChange={handleChanges}
-            />
-            <span>|</span>
-            <br/>
-            <span>Pages: |</span>
-            <button onClick={() => setPage(1)}>1</button>
-            <button onClick={() => setPage(2)}>2</button>
-            <button onClick={() => setPage(3)}>3</button>
-            <button onClick={() => setPage(4)}>4</button>
-            <button onClick={() => setPage(5)}>5</button>
-            <button onClick={() => setPage(6)}>6</button>
-            <button onClick={() => setPage(7)}>7</button>
-            <button onClick={() => setPage(8)}>8</button>
-            <button onClick={() => setPage(9)}>9</button>
-            <button onClick={() => setPage(10)}>10</button>
-            <button onClick={() => setPage(11)}>11</button>
-            <button onClick={() => setPage(12)}>12</button>
-            <button onClick={() => setPage(13)}>13</button>
-            <button onClick={() => setPage(14)}>14</button>
-            <button onClick={() => setPage(15)}>15</button>
-            <button onClick={() => setPage(16)}>16</button>
-            <button onClick={() => setPage(17)}>17</button>
-            <button onClick={() => setPage(18)}>18</button>
-            <button onClick={() => setPage(19)}>19</button>
-            <button onClick={() => setPage(20)}>20</button>
-            <span>|</span>
+        <section className="container">
+            <div className="forms">
+                <div>
+                    <span>Character: </span>
+                    <input
+                        type='text'
+                        placeholder='Search'
+                        value={searchTerm}
+                        onChange={handleChanges}
+                    />
+                </div>
+                <div>
+                    <span>Pages |</span>
+                    <button onClick={() => setPage(1)}>1</button>
+                    <button onClick={() => setPage(2)}>2</button>
+                    <button onClick={() => setPage(3)}>3</button>
+                    <button onClick={() => setPage(4)}>4</button>
+                    <button onClick={() => setPage(5)}>5</button>
+                    <button onClick={() => setPage(6)}>6</button>
+                    <button onClick={() => setPage(7)}>7</button>
+                    <button onClick={() => setPage(8)}>8</button>
+                    <button onClick={() => setPage(9)}>9</button>
+                    <button onClick={() => setPage(10)}>10</button>
+                    <button onClick={() => setPage(11)}>11</button>
+                    <button onClick={() => setPage(12)}>12</button>
+                    <button onClick={() => setPage(13)}>13</button>
+                    <button onClick={() => setPage(14)}>14</button>
+                    <button onClick={() => setPage(15)}>15</button>
+                    <button onClick={() => setPage(16)}>16</button>
+                    <button onClick={() => setPage(17)}>17</button>
+                    <button onClick={() => setPage(18)}>18</button>
+                    <button onClick={() => setPage(19)}>19</button>
+                    <button onClick={() => setPage(20)}>20</button>
+                    <span>|</span>
+                </div>
+            </div>
             <div className="characters">
                 {searchResults.map((data, index) => (
                     <Character
@@ -78,7 +82,7 @@ function CardContainer() {
                     />
                 ))}
             </div>
-        </div>
+        </section>
 
 
 
