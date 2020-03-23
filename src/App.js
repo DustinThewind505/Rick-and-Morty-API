f5af03f11c35d828d195893218a13c7a642b5a7d
unknown <Dustin.Guillen14@yahoo.com>
2020-03-22 23:07:50 -0600

    changed backgrounds to green portal

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header" style={background ? { background: 'url("https://wallpaperboat.com/wp-content/uploads/2019/04/rick-and-morty-wallpaper-portal-wallpaper-001.jpg")' } : { background: "#282c34" }} onClick={bgColor}>
+      <header className="App-header" style={background ? { background: "#282c34" } : { background: 'url("https://wallpaperboat.com/wp-content/uploads/2019/04/rick-and-morty-wallpaper-portal-wallpaper-001.jpg")' }} onClick={bgColor}>
 
         <img src="https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://img.cartoongoodies.com/wp-content/uploads/2019/11/07152740/Rick-Sanchez-face-778x1024.png"} />
 
