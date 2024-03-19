537492afb5adfb76e1efd7ead745fe50f36d0c58
unknown <Dustin.Guillen14@yahoo.com>
2024-03-18 22:37:31 -0600

    [ticket-number] aaded images to local file tree

--- src/App.js
+++ src/App.js
@@ -14,13 +14,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header" style={background ? { background: "#282c34" } : { background: 'url("https://wallpaperboat.com/wp-content/uploads/2019/04/rick-and-morty-wallpaper-portal-wallpaper-001.jpg")' }} onClick={bgColor}>
+      <header className="App-header" style={background ? { background: 'url("images/rick-and-morty-wallpaper2.jpg")', backgroundPosition: "50% 50%", backgroundSize: "cover" } : { background: 'url("images/rick-and-morty-wallpaper.jpg")' }} onClick={bgColor}>
 
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a46868a6-3de3-4c82-b291-dcc524c1a7a7/d9mxrw7-a76a3243-56ed-4314-8dcd-ded2078dc26b.png/v1/fill/w_831,h_962,q_75,strp/rick_face_by_kushmastafresh-d9mxrw7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9hNDY4NjhhNi0zZGUzLTRjODItYjI5MS1kY2M1MjRjMWE3YTcvZDlteHJ3Ny1hNzZhMzI0My01NmVkLTQzMTQtOGRjZC1kZWQyMDc4ZGMyNmIucG5nIiwid2lkdGgiOiI8PTgzMSIsImhlaWdodCI6Ijw9OTYyIn1dXX0.SjC7iCqi43Ux7hEArOv0VnaWwVWdJpvOricTa6Mms0Q" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "https://media.dayoftheshirt.com/images/shirts/TCyaG/popvulture_evil-morty-pin_1514855690.full.png"} onMouseLeave={(event) => event.target.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a46868a6-3de3-4c82-b291-dcc524c1a7a7/d9mxrw7-a76a3243-56ed-4314-8dcd-ded2078dc26b.png/v1/fill/w_831,h_962,q_75,strp/rick_face_by_kushmastafresh-d9mxrw7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9hNDY4NjhhNi0zZGUzLTRjODItYjI5MS1kY2M1MjRjMWE3YTcvZDlteHJ3Ny1hNzZhMzI0My01NmVkLTQzMTQtOGRjZC1kZWQyMDc4ZGMyNmIucG5nIiwid2lkdGgiOiI8PTgzMSIsImhlaWdodCI6Ijw9OTYyIn1dXX0.SjC7iCqi43Ux7hEArOv0VnaWwVWdJpvOricTa6Mms0Q"} />
+        <img src="images/rick-sanchez-head.png" className="App-logo" alt="Spinning Rick Sanchez head" onMouseOver={(event) => event.target.src = "images/jerrySmithHead.png"} onMouseLeave={(event) => event.target.src = "images/rick-sanchez-head.png"} />
 
-        <h1 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
+        {
+          !background ? <h1 onMouseOver={(event) => event.target.style.color = "yellow"} onMouseLeave={(event) => event.target.style.color = "white"}>
           "Wubba Lubba Dub Dub!" -Rick Sanchez
-        </h1>
+        </h1> : null
+        }
 
       </header>
       <div className="navbar">
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,14 +3,13 @@ import React from "react";
 
 
 function CharacterCard(props) {
-
     
 
     return (
         <div className="character-list">
             <div key={props.id}>
                 <h2 onMouseOver={(event) => event.target.textContent = "Evil Morty 🚫"} onMouseLeave={(event) => event.target.textContent = props.name}>{props.name}</h2>
-                <img src={props.image} className='test' onMouseOver={(event) => event.target.src = "https://rickandmortyapi.com/api/character/avatar/118.jpeg"} onMouseLeave={(event) => event.target.src = props.image} alt={props.name}/>
+                <img src={props.image} className='test' onMouseOver={(event) => event.target.src = "images/evil-morty-profile-image.jpeg"} onMouseLeave={(event) => event.target.src = props.image} alt={props.name}/>
                 <h4>Location: {props.location.name}</h4>
                 <h4>DOA: <span className="doa">{props.status}</span></h4>
             </div>
