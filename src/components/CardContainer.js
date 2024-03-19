96bb4c0b82b964aae3796fca3f2be53c66485aa1
unknown <Dustin.Guillen14@yahoo.com>
2024-03-19 13:43:05 -0600

    [ticket-number] polished code for page navigation top and bottom of characters page

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -7,6 +7,7 @@ import "./CardContainer.css";
 
 function CardContainer() {
     const [page, setPage] = useState(1);
+    const [numberOfPages, setNumberOfPages] = useState(0);
 
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([])
@@ -19,19 +20,19 @@ function CardContainer() {
         axios
             .get(`https://rickandmortyapi.com/api/character?page=${page}`)
             .then(response => {
-                console.log(response)
                 const results = response.data.results.filter(element =>
                     element.name.toLowerCase().includes(searchTerm.toLowerCase())
                 );
 
                 setSearchResults(results)
-                //setRickMorty(response.data.results);
+                setNumberOfPages(response.data.info.pages)
             })
             .catch(error => {
                 console.log("The data was not returned", error);
             })
 
-    }, [searchTerm, page])
+    }, [searchTerm, page, numberOfPages])
+
 
 
     return (
@@ -48,26 +49,14 @@ function CardContainer() {
                 </div>
                 <div>
                     <span>Pages |</span>
-                    <button onClick={() => setPage(1)}>1</button>
-                    <button onClick={() => setPage(2)}>2</button>
-                    <button onClick={() => setPage(3)}>3</button>
-                    <button onClick={() => setPage(4)}>4</button>
-                    <button onClick={() => setPage(5)}>5</button>
-                    <button onClick={() => setPage(6)}>6</button>
-                    <button onClick={() => setPage(7)}>7</button>
-                    <button onClick={() => setPage(8)}>8</button>
-                    <button onClick={() => setPage(9)}>9</button>
-                    <button onClick={() => setPage(10)}>10</button>
-                    <button onClick={() => setPage(11)}>11</button>
-                    <button onClick={() => setPage(12)}>12</button>
-                    <button onClick={() => setPage(13)}>13</button>
-                    <button onClick={() => setPage(14)}>14</button>
-                    <button onClick={() => setPage(15)}>15</button>
-                    <button onClick={() => setPage(16)}>16</button>
-                    <button onClick={() => setPage(17)}>17</button>
-                    <button onClick={() => setPage(18)}>18</button>
-                    <button onClick={() => setPage(19)}>19</button>
-                    <button onClick={() => setPage(20)}>20</button>
+                    {
+                        [...Array(numberOfPages)].map((_, index) => {
+                            index += 1;
+                            return (
+                                <button key={index} onClick={() => setPage(index)}>{index}</button>
+                            )
+                        })
+                    }
                     <span>|</span>
                 </div>
             </div>
@@ -84,26 +73,14 @@ function CardContainer() {
             </div>
             <div className="footer-buttons">
                     <span>Pages |</span>
-                    <button onClick={() => setPage(1)}>1</button>
-                    <button onClick={() => setPage(2)}>2</button>
-                    <button onClick={() => setPage(3)}>3</button>
-                    <button onClick={() => setPage(4)}>4</button>
-                    <button onClick={() => setPage(5)}>5</button>
-                    <button onClick={() => setPage(6)}>6</button>
-                    <button onClick={() => setPage(7)}>7</button>
-                    <button onClick={() => setPage(8)}>8</button>
-                    <button onClick={() => setPage(9)}>9</button>
-                    <button onClick={() => setPage(10)}>10</button>
-                    <button onClick={() => setPage(11)}>11</button>
-                    <button onClick={() => setPage(12)}>12</button>
-                    <button onClick={() => setPage(13)}>13</button>
-                    <button onClick={() => setPage(14)}>14</button>
-                    <button onClick={() => setPage(15)}>15</button>
-                    <button onClick={() => setPage(16)}>16</button>
-                    <button onClick={() => setPage(17)}>17</button>
-                    <button onClick={() => setPage(18)}>18</button>
-                    <button onClick={() => setPage(19)}>19</button>
-                    <button onClick={() => setPage(20)}>20</button>
+                    {
+                        [...Array(numberOfPages)].map((_, index) => {
+                            index += 1;
+                            return (
+                                <button key={index} onClick={() => setPage(index)}>{index}</button>
+                            )
+                        })
+                    }
                     <span>|</span>
                 </div>
         </section>
