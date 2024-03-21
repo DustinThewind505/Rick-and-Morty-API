3072f81154bdb9d3c17da27ddfcdc3acf84d4e2f
unknown <Dustin.Guillen14@yahoo.com>
2024-03-20 22:21:39 -0600

    [ticket-number] added sample data, added functionality to display data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import seasonData from "../data";
 
 function Home(){
 
@@ -10,12 +12,44 @@ function Home(){
                 <a href="https://www.adultswim.com/streams/rick-and-morty" rel="noopener noreferrer" target="_blank" >FREE 24/7 MARATHON</a>
             </div>
             <div>
-                <div> Season 7</div>
-                <div> Season 6</div>
-                <div> Season 5</div>
-                <div> Season 4</div>
-                <div> Season 3</div>
-                <div> Season 2</div>
+                {
+                    seasonData && seasonData.length > 0 ? 
+                    seasonData.map(season => <div>
+                       <h2>Season: {season.seasonNumber}</h2>
+                       {season.availableEpisodes && season.availableEpisodes.length > 0 ?
+                            season.availableEpisodes.map(episode => <a href={episode.link}>
+                                <div>
+                                    <p>{episode.title}</p>
+                                    <p>{episode.description}</p>
+                                </div>
+                            </a>)
+                            :
+                            <p>No episode data found</p>
+                        }
+                    </div>)
+                    :
+                    <p>No season data found</p>
+                }
+                <div>
+                    <p>Season 6</p>
+                    <span>+</span>
+                </div>
+                <div>
+                    <p>Season 5</p>
+                    <span>+</span>
+                </div>
+                <div>
+                    <p>Season 4</p>
+                    <span>+</span>
+                </div>
+                <div>
+                    <p>Season 3</p>
+                    <span>+</span>
+                </div>
+                <div>
+                    <p>Season 2</p>
+                    <span>+</span>
+                </div>
             </div>
             <footer>
                 <p>FOOTER</p>
---
+++ src/data.js
@@ -0,0 +1,50 @@
+const seasonData = [
+    {
+        id: 7,
+        seasonNumber: 7,
+        availableEpisodes: [
+            {
+                title: "EP 1 HOW POOPY GOT HIS POOP BACK",
+                description: "Broh, come out with us, you're being so boring, dude.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/how-poopy-got-his-poop-back"
+            },
+            {
+                title: "EP 2 THE JERRICK TRAP",
+                description: "Gotta be mindful, broh. Big brain stuff here.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/the-jerrick-trap"
+            },
+            {
+                title: "EP 3 AIR FORCE WONG",
+                description: "Virginia is for lovers, broh.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/air-force-wong"
+            },
+            {
+                title: "EP 4 THAT'S AMORTE",
+                description: "Broh, that's-a some good spaghetti.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/thats-amorte"
+            },
+            {
+                title: "EP 5 UNMORTRICKEN",
+                description: "Rick and Morty wilding out, broh. They getting up to stuff.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/unmortricken"
+            },
+            {
+                title: "EP 6 RICKFENDING YOUR MORT",
+                description: "Gotta keep those receipts, dawg.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/rickfending-your-mort"
+            },
+            {
+                title: "EP 7 WET KUAT AMORTICAN SUMMER",
+                description: "High school, broh. Morty and Summer hang out in this one.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/wet-kuat-amortican-summer"
+            },
+            {
+                title: "EP 10 FEAR NO MORT",
+                description: "Only thing to fear is fear itself, broh.",
+                link: "https://www.adultswim.com/videos/rick-and-morty/fear-no-mort"
+            }
+        ]
+    }
+];
+
+export default seasonData;
