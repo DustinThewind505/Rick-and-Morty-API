548757d72b8f3adbe33c2422864042de1fe1bcd5
unknown <Dustin.Guillen14@yahoo.com>
2024-03-21 19:54:39 -0600

    [ticket-number] added single selecton accodian functonality, styled seasons & episodes containers

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,46 @@ import React, { useState } from "react";
 import seasonData from "../data";
 
 function Home(){
+    const [seasonAccordianSingleSelection, setSeasonAccordianSingleSelection] = useState(0)
 
+    const handleSingleSelection = (id) => {
 
+        if(seasonAccordianSingleSelection === id) {
+            setSeasonAccordianSingleSelection(0);
+        } else {
+            setSeasonAccordianSingleSelection(id)
+        }
+        
+    }
+
+    
     return(
         <div className="home-container">
             <div className="morty-gifs">
                 <img src="images/rick-morty-logo.png" alt="Rick and Morty logo" />
                 <a href="https://www.adultswim.com/streams/rick-and-morty" rel="noopener noreferrer" target="_blank" >FREE 24/7 MARATHON</a>
             </div>
-            <div>
+            <div className="seasons-container">
                 {
                     seasonData && seasonData.length > 0 ? 
-                    seasonData.map(season => <div>
-                       <h2>Season: {season.seasonNumber}</h2>
-                       {season.availableEpisodes && season.availableEpisodes.length > 0 ?
-                            season.availableEpisodes.map(episode => <a href={episode.link}>
-                                <div>
-                                    <p>{episode.title}</p>
-                                    <p>{episode.description}</p>
-                                </div>
-                            </a>)
-                            :
-                            <p>No episode data found</p>
-                        }
+                    seasonData.map(season => <div className="seasons-card" key={season.id} onClick={() => handleSingleSelection(season.id)}>
+                       <div className="seasons-card-title">
+                            <h2>Season: {season.seasonNumber}</h2>
+                            <p>{seasonAccordianSingleSelection !== season.id ? "+" : "-"}</p>
+                       </div>
+                       <div className="episodes-container">
+                            {seasonAccordianSingleSelection === season.id && season.availableEpisodes.length > 0 ?
+                                season.availableEpisodes.map(episode => <a key={episode.id} href={episode.link} rel="noopener noreferrer" target="_blank">
+                                    <div>
+                                        <p>{episode.title}</p>
+                                        <p>{episode.description}</p>
+                                    </div>
+                                </a>
+                                )
+                                :
+                                null
+                            }
+                       </div>
                     </div>)
                     :
                     <p>No season data found</p>
--- src/data.js
+++ src/data.js
@@ -4,41 +4,49 @@ const seasonData = [
         seasonNumber: 7,
         availableEpisodes: [
             {
+                id: 1,
                 title: "EP 1 HOW POOPY GOT HIS POOP BACK",
                 description: "Broh, come out with us, you're being so boring, dude.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/how-poopy-got-his-poop-back"
             },
             {
+                id: 2,
                 title: "EP 2 THE JERRICK TRAP",
                 description: "Gotta be mindful, broh. Big brain stuff here.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/the-jerrick-trap"
             },
             {
+                id: 3,
                 title: "EP 3 AIR FORCE WONG",
                 description: "Virginia is for lovers, broh.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/air-force-wong"
             },
             {
+                id: 4,
                 title: "EP 4 THAT'S AMORTE",
                 description: "Broh, that's-a some good spaghetti.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/thats-amorte"
             },
             {
+                id: 5,
                 title: "EP 5 UNMORTRICKEN",
                 description: "Rick and Morty wilding out, broh. They getting up to stuff.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/unmortricken"
             },
             {
+                id: 6,
                 title: "EP 6 RICKFENDING YOUR MORT",
                 description: "Gotta keep those receipts, dawg.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/rickfending-your-mort"
             },
             {
+                id: 7,
                 title: "EP 7 WET KUAT AMORTICAN SUMMER",
                 description: "High school, broh. Morty and Summer hang out in this one.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/wet-kuat-amortican-summer"
             },
             {
+                id: 10,
                 title: "EP 10 FEAR NO MORT",
                 description: "Only thing to fear is fear itself, broh.",
                 link: "https://www.adultswim.com/videos/rick-and-morty/fear-no-mort"
