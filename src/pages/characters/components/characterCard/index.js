f46eca00c68a072d9a1d0e53428b781c99a85e3d
Dustin G <Dustin.Guillen14@yahoo.com>
2024-05-13 15:44:00 -0600

    [ticket-number] updated class names on characters page, updated css on character card

--- src/pages/characters/components/characterCard/index.js
+++ src/pages/characters/components/characterCard/index.js
@@ -6,7 +6,7 @@ function CharacterCard(props) {
     
 
     return (
-        <div key={props.id} className="character-list">
+        <div key={props.id} className="character-card">
             <h2>{props.name}</h2>
             <img className='test' src={props.image} alt={props.name}/>
             <p>Location: {props.location?.name}</p>
